b6e9d18fa6e11272dd80ef50cd5f5cb94e851bd5
Cristian Contreras <contreras.cristian994@gmail.com>
2025-11-24 00:27:55 -0500

    jwt token creation on signup

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcryptjs";
 import User from "../models/user.model.js";
+import generateTokenAndSetCookie from "../utils/generateToken.js";
 
 export const signup = async (req, res) => {
     try {
@@ -18,8 +19,8 @@ export const signup = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         
-        const maleProfilePic = `https://avatar.iran.liara.run/public/boy`;
-        const femaleProfilePic = `https://avatar.iran.liara.run/public/girl`;
+        const maleProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+        const femaleProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
 
         const newUser = new User({
             fullName,
@@ -29,15 +30,19 @@ export const signup = async (req, res) => {
             profilePic: gender === "male" ? maleProfilePic : femaleProfilePic
         });
 
-        await newUser.save();
-
-        res.status(201).json({
-            _id: newUser._id,
-            fullName: newUser.fullName,
-            username: newUser.username,
-            gender: newUser.gender,
-            profilePic: newUser.profilePic
-        });
+        if(newUser) {
+            generateTokenAndSetCookie(newUser._id, res);
+            await newUser.save();
+            res.status(201).json({
+                _id: newUser._id,
+                fullName: newUser.fullName,
+                username: newUser.username,
+                gender: newUser.gender,
+                profilePic: newUser.profilePic
+            });
+        } else {
+            res.status(400).json({ error: "Invalid user data" });
+        }
 
     } catch (error) {
         console.log("Error in signup controller ", error.message);
