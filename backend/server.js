71d40aac504de5dc38b0cdce31b0459b705c3c9d
Cristian Contreras <contreras.cristian994@gmail.com>
2025-11-28 03:04:47 -0500

    getUsers route added

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import connectToDB from "./config/db.js";
 import authRouter from "./routes/auth.routes.js";
 import messageRouter from "./routes/message.routes.js";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 const PORT =  process.env.PORT || 5000;
@@ -14,6 +15,7 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRouter);
 app.use("/api/messages", messageRouter);
+app.use("/api/users", userRouter);
 
 app.get("/", (req, res) => {
     res.send("Hello world!!");
